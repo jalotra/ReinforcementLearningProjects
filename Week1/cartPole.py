f3bfb9d4156e9c738a9709658a341b131f78ac82
Shivam <jalotrashivam9@gmail.com>
2019-08-08 10:26:33 +0530

    CartPOle Balaning ACT

---
+++ Week1/cartPole.py
@@ -0,0 +1,41 @@
+import numpy as np
+import gym
+
+def play(env, policy):
+    observation = env.reset()
+
+    done = False
+    score = 0
+    observations = []
+    
+
+    def mainLoop(observation, done, score):
+        for _ in range(5000):
+            observations = list(observation) 
+
+            if done:
+                break
+
+            outcome = np.dot(observations, np.dot(1, policy).flatten()) 
+
+            if outcome > 0:
+                action = 1
+            else:
+                action = 0
+            
+            observation, reward, done, info = env.step(action)
+            score += reward
+
+        return score, observations
+    
+    return mainLoop(observation, done, score)
+
+def Epochs(number_of_epochs, env, policy):
+    for _ in range(number_of_epochs):
+        play(env, policy)[0]
+
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    CurrentPolicy = np.random.randn(1, 4)
+    print('Policy Score' , Epochs(10, env, CurrentPolicy))
